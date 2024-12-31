3b9d16509a6f0ba154b401fdc8d659c139d1c98f
devadore <samuelmendestcsom@outlook.com>
2024-12-31 17:40:37 -0300

    criando commits deste projeto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import styles from './header.module.css'
+import logoimg from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+
+export function Header(){
+    return(
+        <header className={styles.container}>
+            <Link to="/">
+            <img src={logoimg} alt="logo cripto app" />
+            
+            </Link>
+        </header>
+    )
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,10 @@
+export function Notfound() {
+
+    return (
+      <div>
+          <h1>Pagina 404, esta pagina não existe</h1>
+  
+      </div>
+    )
+  }
+  
